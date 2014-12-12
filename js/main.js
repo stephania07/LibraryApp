0d8f9b9bc3a99c5c62b43809a46e60cafe55bb55
Stephania <stephania07>
2014-12-11 21:56:59 -0600

    show, edit, remove, add buttons function

--- js/main.js
+++ js/main.js
@@ -18,20 +18,74 @@
         controller: 'ShowController',
         controllerAs: 'show'
       })
+      .when('/:id/edit', {
+        templateUrl: 'views/form.html',
+        controller: 'EditController',
+        controllerAs: 'lib'
+      })
       .otherwise({redirectTo: '/'});
     })
-    .controller("ShowController", function($http, $routeParams) {
-    var vm = this;
-    var id = $routeParams.id;
-     $http.get("https://librarybookapp.firebaseio.com/books/" + id + ".json")
+    .factory('libFactory', function($http){
+      function showLib(id, cb) {
+      $http.get("https://librarybookapp.firebaseio.com/books/" + id + ".json")
+      .success(function(data){
+        cb(data);
+      })
+      .error(function(err){
+        console.log(err);
+      });
+     }
+     
+     function getLib(id, cb) {
+       var url = "https://librarybookapp.firebaseio.com/books/" + id + ".json"
+       $http.get(url)
        .success(function(data){
-          vm.books = data;
+         cb(data);
        })
        .error(function(err){
          console.log(err);
-     })
+       });
+     }
+     return {
+       showLib: showLib,
+       getLib: getLib
+     };
+   })
+  
+    .controller("ShowController", function($routeParams, libFactory) {
+      var vm = this;
+      var id = $routeParams.id;
+      libFactory.getLib(id, function(data){
+          vm.book =data;
+        });
     })
-  .controller("LibraryAppController", function($http) {
+        
+    .controller('EditController', function($http, $routeParams, $location, libFactory){
+    var vm = this;
+    var id = $routeParams.id;
+    var url = "https://librarybookapp.firebaseio.com/books/" + id + ".json"
+   
+    libFactory.getLib(id, function(data){
+      vm.newBook = data;
+    });
+
+    vm.addNewBook = function(){
+      $http.put(url, vm.newBook)
+        .success(function(data){
+          $location.path('/')
+        })
+        .error(function(err){
+          console.log(err);
+        })
+    };
+
+    vm.categoryOptions ={
+      fiction: 'Fiction',
+      science: 'Science',
+      biography: 'Biography'
+    };
+  })
+    .controller("LibraryAppController", function($http) {
     var vm = this;
     
      $http.get("https://librarybookapp.firebaseio.com/books.json")
